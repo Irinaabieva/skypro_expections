ca19dcb310c884d9245ce5ee3b19d3dfea0835e9
VarzievE <edvarziev@gmail.com>
2022-12-25 12:30:02 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
+        boolean succes = Data.validate("test", "test!", "test11");
+        if (succes) {
+            System.out.println("Данные валидны");
+        } else {
+            System.out.println("Данные не валидны");
+        }
     }
 }
